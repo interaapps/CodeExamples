050801adaa1beac14b50f195b158460fac6d2207
JulianFun123 <julianfun123@yahoo.com>
2019-06-21 10:50:04 +0200

    Hello world!

--- Java/Functions.java
+++ Java/Functions.java
@@ -1,7 +1,7 @@
 public class Functions {
-
+    
     public static void main(String[] args) {
-        Functions.examplePrintFunction("Hello world!");
+        examplePrintFunction("Hello world!");
     }
     
     public static void examplePrintFunction(String print) {
